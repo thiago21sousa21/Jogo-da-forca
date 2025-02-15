d1165bdc7092eef97eadf964eba7163bb979b34e
thiago21sousa21 <thiago21sousa21@gmail.com>
2025-02-15 00:53:38 -0300

    refactor[jogos.js]: diminuindo imports

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+import forca0 from './forca0.png'
+import forca1 from './forca1.png'
+import forca2 from './forca2.png'
+import forca3 from './forca3.png'
+import forca4 from './forca4.png'
+import forca5 from './forca5.png'
+import forca6 from './forca6.png'
+
+const imagens = [
+    forca0, forca1, forca2, forca3, forca4, forca5, forca6,
+]
+export default imagens
--- src/components/Jogo/Jogo.js
+++ src/components/Jogo/Jogo.js
@@ -1,15 +1,9 @@
-import  forca0 from'../../assets/forca0.png';
-import  forca1 from'../../assets/forca1.png';
-import  forca2 from'../../assets/forca2.png';
-import  forca3 from'../../assets/forca3.png';
-import  forca4 from'../../assets/forca4.png';
-import  forca5 from'../../assets/forca5.png';
-import  forca6 from'../../assets/forca6.png';
 
+import imagens from './../../assets/index.js'
 
 export default function Jogo(props){
-  const arrayImagens = [ forca0, forca1, forca2, forca3, forca4, forca5, forca6 ];
 
+  console.log('IMAGENS', imagens)
 
 let {palavras, 
   palavra, setPalavra, 
@@ -36,7 +30,7 @@ let {palavras,
 
     return(
       <div className="Jogo">
-        <img data-test="game-image" className="imgForca" src={arrayImagens[cont]}/>
+        <img data-test="game-image" className="imgForca" src={imagens[cont]}/>
         <div className="contBotaoPalavra">
           <button data-test="choose-word" onClick={sortearPalavra}  className="botaoInicio">Escolher Palavra</button>
           <div className="palavra"><h1 data-test="word" className={cor}>{palavra}</h1></div>
